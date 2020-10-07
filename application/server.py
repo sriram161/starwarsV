b9592f11de46b518bf3b26eb5360f1ce848b277b
sriram161 <k.sriramsagar@gmail.com>
2020-10-06 21:21:56 -0400

    updated params of start.

--- application/server.py
+++ application/server.py
@@ -29,8 +29,8 @@ class Server(object):
     def create_all_tables(self):
         self.db.create_all()
 
-    def start(self, debug=False, port=8080):
-        self.app.run(debug=debug, port=port)
+    def start(self, debug_=False, port_=8080):
+        self.app.run(debug=debug_, port=port_)
 
     def setup_api(self):
         self.api = Api(self.app, version='1.0', title='Starwars films.', description='get all the starwars films.')
